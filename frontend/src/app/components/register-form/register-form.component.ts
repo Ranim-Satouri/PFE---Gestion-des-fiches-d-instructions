90b08d42509665ce3587be2e97985408267f4c06
Abdellatif Ranim <ranimabdellatif5@gmail.com>
2025-03-30 05:19:27 +0100

    forms done :D

--- frontend/src/app/components/register-form/register-form.component.ts
+++ frontend/src/app/components/register-form/register-form.component.ts
@@ -11,6 +11,20 @@ import { CommonModule } from '@angular/common';
 
 })
 export class RegisterFormComponent {
+  steps = ["Informations personnelles", "Info du compte", "Statut et rôle"];
+  currentStep = 1; // Changer dynamiquement en fonction de l'avancement
+  // Méthodes pour avancer et reculer
+  goToNextStep() {
+    if (this.currentStep < 3) {  // Changez ce nombre selon le nombre de steps
+      this.currentStep++;
+    }
+  }
+
+  goToPreviousStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
   @Output() close = new EventEmitter<void>();
   @Output() submit = new EventEmitter<any>();
 
